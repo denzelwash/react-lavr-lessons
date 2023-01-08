d2dafa8eadd25279add0593feb77aacf4cfa1dc1
Denis Obolevich <denzelwash33@mail.ru>
2023-01-08 16:52:26 +0300

    l2 hw

--- react-lesson2-src/src/App.js
+++ react-lesson2-src/src/App.js
@@ -1,70 +1,85 @@
 import React, { useState } from 'react'
-import MinMax from './MinMax';
+import MinMax from './MinMax'
 
-export default function(){
-	let [ products, setProducts ] = useState(productsStub());
+export default function () {
+	let [products, setProducts] = useState(productsStub())
 
 	let setCnt = (id, cnt) => {
-		setProducts(products.map(pr => pr.id != id ? pr : ({ ...pr, cnt })));
+		setProducts(products.map((pr) => (pr.id != id ? pr : { ...pr, cnt })))
 	}
 
-	return <div className="some">
-		<h1>Products list</h1>
-		<table>
-			<tbody>
-				<tr>
-					<th>#</th>
-					<th>Title</th>
-					<th>Price</th>
-					<th>Cnt</th>
-					<th>Total</th>
-				</tr>
-				{ products.map((pr, i) => (
-					<tr key={pr.id}>
-						<td>{ i + 1 }</td>
-						<td>{ pr.title }</td>
-						<td>{ pr.price }</td>
-						<td>
-							<MinMax max={pr.rest} current={pr.cnt} onChange={cnt => setCnt(pr.id, cnt)} />
-						</td>
+	let delProduct = (id) => {
+		setProducts([...products.filter((pr) => pr.id !== id)])
+	}
+
+	const total = () => {
+		const total = products.reduce((sum, current) => sum + current.price * current.cnt, 0)
+		return total
+	}
+
+	return (
+		<div className="some">
+			<h1>Products list</h1>
+			<table>
+				<tbody>
+					<tr>
+						<th>#</th>
+						<th>Title</th>
+						<th>Price</th>
+						<th>Cnt</th>
+						<th>Total</th>
+						<th></th>
 					</tr>
-				)) }
-			</tbody>
-		</table>
-	</div>;
+					{products.map((pr, i) => (
+						<tr key={pr.id}>
+							<td>{i + 1}</td>
+							<td>{pr.title}</td>
+							<td>{pr.price}</td>
+							<td>
+								<MinMax max={pr.rest} current={pr.cnt} onChange={(cnt) => setCnt(pr.id, cnt)} />
+							</td>
+							<td>{pr.price * pr.cnt}</td>
+							<td onClick={() => delProduct(pr.id)}>Удалить</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<p>Total: {total()}</p>
+		</div>
+	)
 }
 
-function productsStub(){
+function productsStub() {
 	return [
 		{
 			id: 100,
 			title: 'Ipnone 200',
 			price: 12000,
 			rest: 10,
-			cnt: 1
+			cnt: 1,
 		},
 		{
 			id: 101,
 			title: 'Samsung AAZ8',
 			price: 22000,
 			rest: 5,
-			cnt: 1
+			cnt: 1,
 		},
 		{
 			id: 103,
 			title: 'Nokia 3310',
 			price: 5000,
 			rest: 2,
-			cnt: 1
+			cnt: 1,
 		},
 		{
 			id: 105,
 			title: 'Huawei ZZ',
 			price: 15000,
 			rest: 8,
-			cnt: 1
-		}
-	];
+			cnt: 1,
+		},
+	]
 }
 
 /* 
--- react-lesson2-src/src/MinMax.js
+++ react-lesson2-src/src/MinMax.js
@@ -1,32 +1,54 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 MinMax.propTypes = {
 	min: PropTypes.number,
 	max: PropTypes.number.isRequired,
 	current: PropTypes.number.isRequired,
-	onChange: PropTypes.func.isRequired
+	onChange: PropTypes.func.isRequired,
 }
 
-function MinMax({ min = 1, max, current, onChange }){
-	function applyCurrent(num){
-		let validNum = Math.max(min, Math.min(max, num));
-		onChange(validNum);
+function MinMax({ min = 1, max, current, onChange }) {
+	let [tempCurrent, setTempCurrent] = useState(current)
+
+	function applyCurrent(num) {
+		let validNum = Math.max(min, Math.min(max, num))
+		onChange(validNum)
 	}
 
-	function parseCurrentStr(e){
-		let num = parseInt(e.target.value);
-		applyCurrent(isNaN(num) ? min : num);
+	// function parseCurrentStr(e) {
+	// 	let num = parseInt(e.target.value)
+	// 	applyCurrent(isNaN(num) ? min : num)
+	// }
+
+	function setTempValue(e) {
+		let num = e.target.value
+		if (num === '') {
+			setTempCurrent(min)
+		} else if (!isNaN(num)) {
+			setTempCurrent(num)
+		}
 	}
 
-	let inc = () => applyCurrent(current + 1);
-	let dec = () => applyCurrent(current - 1);
+	useEffect(() => {
+		setTempCurrent(current)
+	}, [current])
 
-	return <div>
-		<button type="button" onClick={ dec }>-</button>
-		<input type="text" value={ current } onChange={parseCurrentStr} />
-		<button type="button" onClick={ inc }>+</button>
-	</div>
+	let inc = () => applyCurrent(current + 1)
+	let dec = () => applyCurrent(current - 1)
+
+	return (
+		<div>
+			<button type="button" onClick={dec}>
+				-
+			</button>
+			{current}
+			<input type="text" value={tempCurrent} onChange={setTempValue} onBlur={() => applyCurrent(tempCurrent)} />
+			<button type="button" onClick={inc}>
+				+
+			</button>
+		</div>
+	)
 }
 
-export default MinMax;
+export default MinMax
